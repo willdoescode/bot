7f97d27c0c2567bd40f1f4d45a7bd6eb0ff9bd2a
Will Iam <williamlane923@gmail.com>
2020-03-01 20:36:08 -0800

    configed logging

--- cogs/errorcatch.py
+++ cogs/errorcatch.py
@@ -4,7 +4,7 @@ import logging
 
 logging.basicConfig(
 	format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-	level=logging.DEBUG
+	level=logging.INFO
 )
 logger = logging.getLogger(__name__)
 
